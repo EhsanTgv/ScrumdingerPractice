e848f8cab15469cd33fb7ca9302ef3baf6473e7f
EhsanTgv <EhsanTgv@gmail.com>
2022-03-08 15:59:05 +0300

    Initial Commit

---
+++ ScrumdingerPractice/ScrumdingerPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerPracticeApp.swift
+//  ScrumdingerPractice
+//
+//  Created by İbrahim on 8.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
